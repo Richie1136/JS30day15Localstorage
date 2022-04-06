3163d4e352c9befae07570934b9ecb5fa00da2da
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-06 13:41:19 -0400

    Using this.reset to clear the form after i submit

--- main.js
+++ main.js
@@ -7,11 +7,12 @@ function addItem(event) {
   event.preventDefault()
   // Could use this becuase that is the form tag
   const text = (this.querySelector('[name=item]')).value
-  console.log(text)
   const item = {
     text: text,
     done: false
   }
+  console.log(item)
+  this.reset()
 }
 
 addItems.addEventListener('submit', addItem)

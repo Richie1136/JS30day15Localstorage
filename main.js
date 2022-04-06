ca83500a0b01853260f1900c4b219414e085caa3
Richie Hagenah <69161935+Richie1136@users.noreply.github.com>
2022-04-06 13:26:29 -0400

    Added the starter file styles

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 const addItems = document.querySelector('.add-items')
 const itemsList = document.querySelector('.plates')
 const items = []
+
+
+const addItem = (event) => {
+  console.log("Hello")
+}
+
+addItems.addEventListener('submit', addItem)
